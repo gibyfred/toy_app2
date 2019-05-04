112b1de0d5b31675994ce231ac7e809a6d388a6e
Giby Cafe2 <gibyfred@gmail.com>
2019-05-04 21:20:58 +0900

    index を貼る

--- db/migrate/20190501095714_create_microposts.rb
+++ db/migrate/20190501095714_create_microposts.rb
@@ -2,9 +2,10 @@ class CreateMicroposts < ActiveRecord::Migration[5.1]
   def change
     create_table :microposts do |t|
       t.text :content
-      t.integer :user_id
+      t.integer :user_id, null: false, index: true
 
       t.timestamps
     end
+
   end
 end
