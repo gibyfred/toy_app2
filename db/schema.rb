ba4d41c71fa23d352380ad60d90bf357f18e69b2
Giby Cafe2 <gibyfred@gmail.com>
2019-05-05 17:43:07 +0900

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -14,9 +14,10 @@ ActiveRecord::Schema.define(version: 20190501095714) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
-    t.integer "user_id"
+    t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_microposts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
